0933f3e122d68af1b59d121594342807c4d71840
Jeroen Galjaard <J.M.Galjaard-1@student.tudelft.nl>
2021-05-25 08:47:26 +0200

    Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# CD-ing into the correct directory
+cd /home/$USER/fltk-testbed-gr-30
+
+# Pull most recent changes
+git pull
+
+# Install the changes
+sudo python3 -m setup.py install
+
+
