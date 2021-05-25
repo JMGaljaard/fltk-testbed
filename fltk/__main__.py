a58c7b0313fd6b013d64da5bab02c3352e7a82ed
Jeroen Galjaard <J.M.Galjaard-1@student.tudelft.nl>
2021-05-25 10:37:45 +0200

    Include parser for poisoned attacks

--- fltk/__main__.py
+++ fltk/__main__.py
@@ -25,6 +25,7 @@ def main():
 
     subparsers = parser.add_subparsers(dest="mode")
 
+    # Create single experiment parser
     single_parser = subparsers.add_parser('single')
     single_parser.add_argument('config', type=str)
     single_parser.add_argument('--rank', type=int)
@@ -32,15 +33,26 @@ def main():
     single_parser.add_argument('--host', type=str, default=None)
     add_default_arguments(single_parser)
 
+    # Create spawn parser
     spawn_parser = subparsers.add_parser('spawn')
     spawn_parser.add_argument('config', type=str)
     add_default_arguments(spawn_parser)
 
+    # Create remote parser
     remote_parser = subparsers.add_parser('remote')
     remote_parser.add_argument('--rank', type=int)
     remote_parser.add_argument('--nic', type=str, default=None)
     remote_parser.add_argument('--host', type=str, default=None)
     add_default_arguments(remote_parser)
+
+    # Create poisoned parser
+    poison_parser = subparsers.add_parser('poison')
+    poison_parser.add_argument('config', type=str)
+    poison_parser.add_argument('--rank', type=int)
+    poison_parser.add_argument('--nic', type=str, default=None)
+    poison_parser.add_argument('--host', type=str, default=None)
+    add_default_arguments(remote_parser)
+
     args = parser.parse_args()
     if args.mode == 'remote':
         if args.rank is None or args.host is None or args.world_size is None or args.nic is None:
