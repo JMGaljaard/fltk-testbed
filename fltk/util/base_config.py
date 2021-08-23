f1f12cfb73cfce0f7bb0d155c3c7336d9d2e01f9
Jeroen Galjaard <J.M.Galjaard-1@student.tudelft.nl>
2021-08-23 11:08:32 +0200

    Refactor config and create json dataclasses

--- fltk/util/base_config.py
+++ fltk/util/base_config.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Dict
 
 import torch
 from dataclasses_json import dataclass_json
@@ -9,10 +8,10 @@ from dataclasses_json import dataclass_json
 # SEED = 1
 # torch.manual_seed(SEED)
 
+
 @dataclass
 @dataclass_json
-class ExecutionConfig():
-    cuda: bool = False
+class GeneralNetConfig:
     save_model: bool = False
     save_temp_model: bool = False
     save_epoch_interval: int = 1
@@ -21,14 +20,52 @@ class ExecutionConfig():
     epoch_save_end_suffix = "end"
 
 
+@dataclass(frozen=True)
+@dataclass_json
+class ReproducabilityConfig:
+    torch_seed: int
+    arrival_seed: int
+
+
+@dataclass
+@dataclass_json
+class ExecutionConfig:
+    general_net: GeneralNetConfig
+    reproducability: ReproducabilityConfig
+    experiment_prefix: str = "experiment"
+    tensorboard_active: str = True
+    cuda: bool = False
+
+
+@dataclass
+@dataclass_json
+class OrchestratorConfig:
+    service: str
+    nic: str
+
+
+@dataclass
+@dataclass_json
+class ClientConfig:
+    prefix: str
+
+
+@dataclass
+@dataclass_json
+class ClusterConfig:
+    orchestrator: OrchestratorConfig
+    client: ClientConfig
+    wait_for_clients: bool = True
+
+
 @dataclass
 @dataclass_json
 class BareConfig(object):
     # Configuration parameters for PyTorch and models that are generated.
-    execution_config = ExecutionConfig()
+    execution_config: ExecutionConfig
+    cluster_config: ClusterConfig
 
     def __init__(self):
-
         # TODO: Move to external class/object
         self.train_data_loader_pickle_path = {
             'cifar10': 'data_loaders/cifar10/train_data_loader.pickle',
@@ -47,47 +84,6 @@ class BareConfig(object):
 
         self.default_model_folder_path = "default_models"
         self.data_path = "data"
-
-    def merge_yaml(self, cfg: Dict[str, str] = {}):
-        """
-        @deprecated This function will become redundant after using dataclasses_json to load the values into the object.
-        """
-        if 'total_epochs' in cfg:
-            self.epochs = cfg['total_epochs']
-        if 'epochs_per_cycle' in cfg:
-            self.epochs_per_cycle = cfg['epochs_per_cycle']
-        if 'wait_for_clients' in cfg:
-            self.wait_for_clients = cfg['wait_for_clients']
-        if 'net' in cfg:
-            self.set_net_by_name(cfg['net'])
-        if 'dataset' in cfg:
-            self.dataset_name = cfg['dataset']
-        if 'experiment_prefix' in cfg:
-            self.experiment_prefix = cfg['experiment_prefix']
-        if 'output_location' in cfg:
-            self.output_location = cfg['output_location']
-        if 'tensor_board_active' in cfg:
-            self.tensor_board_active = cfg['tensor_board_active']
-        if 'clients_per_round' in cfg:
-            self.clients_per_round = cfg['clients_per_round']
-        if 'system' in cfg:
-            if 'clients' in cfg['system']:
-                if 'amount' in cfg['system']['clients']:
-                    self.world_size = cfg['system']['clients']['amount'] + 1
-
-        if 'system' in cfg:
-            if 'federator' in cfg['system']:
-                if 'hostname' in cfg['system']['federator']:
-                    self.federator_host = cfg['system']['federator']['hostname']
-        if 'cuda' in cfg:
-            if cfg['cuda']:
-                self.cuda = True
-            else:
-                self.cuda = False
-        if 'sampler' in cfg:
-            self.data_sampler = cfg['sampler']
-        if 'sampler_args' in cfg:
-            self.data_sampler_args = cfg['sampler_args']
 
     def get_dataloader_list(self):
         """
