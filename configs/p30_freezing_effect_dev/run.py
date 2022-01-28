2dcd3fb068171391550f1aadb73cc4075f274bb1
bacox <bartcox93@gmail.com>
2022-01-29 00:18:10 +0100

    Add freezing effect epxeriment

--- configs/p30_freezing_effect_dev/run.py
+++ configs/p30_freezing_effect_dev/run.py
@@ -7,8 +7,20 @@ if __name__ == '__main__':
     generate_docker(name)
     base_path = f'configs/{Path(__file__).parent.name}'
     exp_list = [
+        'fedavg-iid-freeze-0.yaml',
+        'fedavg-iid-freeze-16.yaml',
         'fedavg-iid-freeze-50.yaml',
+        'fedavg-iid-freeze-83.yaml',
+        'fedavg-non-iid-freeze-100.yaml',
+        'fedavg-non-iid-freeze-33.yaml',
+        'fedavg-non-iid-freeze-66.yaml',
         'fedavg-iid-freeze-100.yaml',
+        'fedavg-iid-freeze-33.yaml',
+        'fedavg-iid-freeze-66.yaml',
+        'fedavg-non-iid-freeze-0.yaml',
+        'fedavg-non-iid-freeze-16.yaml',
+        'fedavg-non-iid-freeze-50.yaml',
+        'fedavg-non-iid-freeze-83.yaml',
         ]
     exp_list = [f'{base_path}/exps/{x}' for x in exp_list]
     first_prefix = '--build'
