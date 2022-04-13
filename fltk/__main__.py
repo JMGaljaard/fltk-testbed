198775cecce06d104b8ee3f77fc728a25fa4e416
Jeroen Galjaard <J.M.Galjaard-1@student.tudelft.nl>
2022-04-13 13:56:56 +0200

    Rework main launch function to clarify intent

--- fltk/__main__.py
+++ fltk/__main__.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import logging
+import sys
 from pathlib import Path
 from typing import Optional, Any
 
@@ -11,7 +12,6 @@ from fltk.util.config.arguments import create_all_subparsers
 from fltk.util.generate_experiments import generate, run
 
 
-# TODO: Add description of the function as optional help.
 __run_op_dict = {
     'util-generate': generate,
     'util-run': run,
@@ -24,42 +24,51 @@ __run_op_dict = {
 
 
 def _save_get(args, param) -> Optional[Any]:
-    a = None
+    save_argument = None
     if args is not None and hasattr(args, param):
-        a = args.__dict__[param]
-    print(f"gotten {param} resulted in {a}")
-    return None
+        save_argument = args.__dict__[param]
+    msg = f"Getting {param}: {save_argument}"
+    logging.debug(msg)
+    return save_argument
+
+
+def _get_distributed_config(args) -> Optional[DistributedConfig]:
+    config = None
+    try:
+        with open(args.config, 'r') as config_file:
+            config = DistributedConfig.from_dict(json.load(config_file)) # pylint: disable=no-member
+            config.config_path = Path(args.config)
+    except Exception as excep: # pylint: disable=broad-except
+        msg = f"Failed to get distributed config: {excep}"
+        logging.info(msg)
+    return config
 
 
 def __main__():
+    """
+    Main loop to perform learning (either Federated or Distributed). Note that Orchestrator is part of this setup for
+    now.
+    @return: Nothing.
+    @rtype: None
+    """
     parser = argparse.ArgumentParser(prog='fltk',
                                      description='Experiment launcher for the Federated Learning Testbed (fltk)')
     subparsers = parser.add_subparsers(dest="action", required=True)
     create_all_subparsers(subparsers)
-    """
-    To create your own parser mirror the construction in the 'client_parser' object.
-    Or refer to the ArgumentParser library documentation.
-    """
-
+    # To create your own parser mirror the construction in the 'client_parser' object.
+    # Or refer to the ArgumentParser library documentation.
     args = parser.parse_args()
-    config = None
-    try:
-        with open(args.config, 'r') as config_file:
-            config: DistributedConfig = DistributedConfig.from_dict(json.load(config_file))
-            config.config_path = Path(args.config)
-    except:
-        pass
+    distributed_config = _get_distributed_config(args)
+
     arg_path, conf_path = None, None
     try:
         arg_path = Path(args.path)
-    except Exception as e:
+    except Exception as _: # pylint: disable=broad-except
         print('No argument path is provided.')
     try:
         conf_path = Path(args.config)
-    except Exception as e:
+    except Exception as _: # pylint: disable=broad-except
         print('No configuration path is provided.')
-
-    # TODO: move kwargs into function as extractor
 
     __run_op_dict[args.action](arg_path, conf_path,
                                rank=_save_get(args, 'rank'),
@@ -68,9 +77,9 @@ def __main__():
                                host=_save_get(args, 'host'),
                                prefix=_save_get(args, 'prefix'),
                                args=args,
-                               config=config)
+                               config=distributed_config)
 
-    exit(0)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
