68badae77db06939fdcad7ffa42530cc05df9134
Jeroen Galjaard <J.M.Galjaard-1@student.tudelft.nl>
2022-04-13 14:03:41 +0200

    Update documentation and linter issues in aggregration strategies

--- fltk/strategy/aggregation/__init__.py
+++ fltk/strategy/aggregation/__init__.py
@@ -1,9 +1,20 @@
+from typing import Callable
+
+import torch
+
 from fltk.util.definitions import Aggregations
 from .FedAvg import fed_avg
 from .aggregation import average_nn_parameters, average_nn_parameters_simple
 
 
-def get_aggregation(name: Aggregations):
+def get_aggregation(name: Aggregations) -> Callable[[...], torch.Tensor]:
+    """
+    Helper function to get specific Aggregation class references.
+    @param name: Aggregation class reference.
+    @type name: Aggregations
+    @return: Class reference corresponding to the requested Aggregation definition.
+    @rtype: Type[torch.optim.Optimizer]
+    """
     enum_type = Aggregations(name.value)
     aggregations_dict = {
             Aggregations.fedavg: fed_avg,
