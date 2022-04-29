0fcbe062e5c77b243dd2ddb2357b21330f2e39a5
Jeroen Galjaard <J.M.Galjaard-1@student.tudelft.nl>
2022-04-29 12:51:26 +0200

    Move function to configuration module

--- fltk/util/config/__init__.py
+++ fltk/util/config/__init__.py
@@ -1,2 +1,14 @@
+from fltk.util.config import Config
 from fltk.util.config.distributed_config import DistributedConfig
 from fltk.util.config.config import Config
+
+
+def retrieve_config_network_params(conf: Config, nic=None, host=None):
+    if hasattr(conf, 'system'):
+        system_attr = getattr(conf, 'system')
+        if 'federator' in system_attr:
+            if 'hostname' in system_attr['federator'] and not host:
+                host = system_attr['federator']['hostname']
+            if 'nic' in system_attr['federator'] and not nic:
+                nic = system_attr['federator']['nic']
+    return nic, host
