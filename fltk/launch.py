4dba3c807535deb7390af275af3c154ecccfbec9
Jeroen Galjaard <J.M.Galjaard-1@student.tudelft.nl>
2022-05-10 15:28:44 +0200

    Update naming convention

--- fltk/launch.py
+++ fltk/launch.py
@@ -20,8 +20,8 @@ from fltk.core.federator import Federator
 from fltk.nets.util.reproducability import init_reproducibility, init_learning_reproducibility
 from fltk.util.cluster.client import ClusterManager
 from fltk.util.cluster.worker import should_distribute
-from fltk.util.config import DistributedConfig, Config, retrieve_config_network_params
-from fltk.util.config.arguments import LearningParameters
+from fltk.util.config import DistributedConfig, Config, retrieve_config_network_params, get_learning_param_config
+from fltk.util.config.arguments import DistLearningConfig
 from fltk.util.environment import retrieve_or_init_env, retrieve_env_config
 from fltk.util.task.generator.arrival_generator import SimulatedArrivalGenerator, SequentialArrivalGenerator
 
@@ -35,7 +35,7 @@ launch_signature = Callable[[Path, Path, Optional[Rank], Optional[NIC], Optional
 
 
 def exec_distributed_client(task_id: str, conf: DistributedConfig = None,
-                            learning_params: LearningParameters = None,
+                            learning_params: DistLearningConfig = None,
                             namespace: Namespace = None):
     """
     Helper function to start the execution of the distributed client training loop.
@@ -166,9 +166,7 @@ def launch_client(arg_path: Path, conf_path: Path, rank: Rank, nic: Optional[NIC
     """
     logging.info("Starting in client mode")
 
-    with open(args.experiment_config) as f:
-        learning_params_dict = yaml.safe_load(f)
-        learning_params = LearningParameters.from_dict(learning_params_dict)
+    learning_params = get_learning_param_config(args)
 
     # Set the seed for PyTorch, numpy seed is mostly ignored. Set the `torch_seed` to a different value
     # for each repetition that you want to run an experiment with.
