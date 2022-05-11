935eb37cce1158c7302472d09a23e2c6abc24492
Jeroen Galjaard <J.M.Galjaard-1@student.tudelft.nl>
2022-05-11 11:41:57 +0200

    Update config file to learning_config file

--- fltk/strategy/optimization/__init__.py
+++ fltk/strategy/optimization/__init__.py
@@ -7,7 +7,7 @@ from .fed_prox import FedProx
 from .fed_nova import FedNova
 
 
-def get_optimizer(name: Optimizations) -> Type[torch.optim.Optimizer]:
+def get_optimizer(name: Optimizations, federated: bool = True) -> Type[torch.optim.Optimizer]:
     """
     Helper function to get specific Optimization class references.
     @param name: Optimizer class reference.
@@ -20,7 +20,10 @@ def get_optimizer(name: Optimizations) -> Type[torch.optim.Optimizer]:
             Optimizations.adam: torch.optim.Adam,
             Optimizations.adam_w: torch.optim.AdamW,
             Optimizations.sgd: torch.optim.SGD,
+        }
+    if federated:
+        optimizers.update({
             Optimizations.fedprox: FedProx,
             Optimizations.fednova: FedNova
-        }
+        })
     return optimizers[name]
