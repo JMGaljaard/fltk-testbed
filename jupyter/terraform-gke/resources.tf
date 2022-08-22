37302869b27fd85814dc4c3079968fde6c12832e
JMGaljaard <J.M.Galjaard-1@student.tudelft.nl>
2022-08-22 16:58:35 +0200

    Update to mvp

---
+++ jupyter/terraform-gke/resources.tf
@@ -0,0 +1,14 @@
+# resources.tf
+
+terraform {
+  required_providers {
+    # Interact with the project
+    google = {
+      source = "hashicorp/google"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+  required_version = ">= 1.0"
+}
