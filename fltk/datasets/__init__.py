d4a630e62c851ff94d98f939ea180424f0ce7250
Jeroen M. Galjaard <J.M.Galjaard-1@student.tudelft.nl>
2022-09-06 16:08:51 +0200

    Address pep-8 violations in dataset init

--- fltk/datasets/__init__.py
+++ fltk/datasets/__init__.py
@@ -4,6 +4,7 @@ from fltk.datasets.fashion_mnist import FashionMNISTDataset
 from fltk.datasets.mnist import MNIST
 from fltk.util.config.definitions import Dataset
 
+
 def get_train_loader_path(name: Dataset) -> str:
     paths = {
         Dataset.cifar10: 'data_loaders/cifar10/train_data_loader.pickle',
@@ -22,4 +23,3 @@ def get_test_loader_path(name: Dataset) -> str:
         Dataset.mnist: 'data_loaders/mnist/test_data_loader.pickle',
     }
     return paths[name]
-
