258143de9ccd5d8d7a65fdaf5be648ac61a04875
Jeroen M. Galjaard <J.M.Galjaard@tudelft.nl>
2023-09-26 15:13:40 +0200

    Add imports to module

--- fltk/nets/util/__init__.py
+++ fltk/nets/util/__init__.py
@@ -1,2 +1,3 @@
 from fltk.nets.util.model import save_model, flatten_params, recover_flattened, load_model_from_file
 from fltk.nets.util.evaluation import calculate_class_recall, calculate_class_precision
+from fltk.nets.util.aggregration import average_nn_parameters, drop_local_weights
